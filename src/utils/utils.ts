c926c9c4bd100980cbca9dea7c5ed5a34622fcc8
itachi <enchantinggg4@gmail.com>
2020-11-16 23:13:32 +0300

    removed dev. prefix

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,6 @@
 import { isDev } from './env';
 
-export const frontUrl = isDev ? "http://localhost:3000" : "https://dev.dota2classic.ru";
+export const frontUrl = isDev ? "http://localhost:3000" : "https://dota2classic.ru";
 // export const backUrl = ;
-export const backUrl = isDev ? "http://localhost:6001" : "https://dev.dota2classic.ru/api";
+export const backUrl = isDev ? "http://localhost:6001" : "https://dota2classic.ru/api";
 // export const backUrl = "http://localhost:5002";
