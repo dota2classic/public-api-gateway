e20560fc0d13b860312d09308c2f0102f021fefa
itachi <enchantinggg4@gmail.com>
2021-01-26 23:37:34 +0300

    UserLoggedInEvent - for faster user name avatar resolve

--- src/rest/steam.controller.ts
+++ src/rest/steam.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, Inject, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { frontUrl } from '../utils/utils';
 import { JwtService } from '@nestjs/jwt';
@@ -7,12 +7,17 @@ import { TOKEN_KEY } from '../utils/env';
 import { ApiExcludeEndpoint } from '@nestjs/swagger';
 import { UserRepository } from '../cache/user/user.repository';
 import { Role } from '../gateway/shared-types/roles';
+import { EventBus } from '@nestjs/cqrs';
+import { UserLoggedInEvent } from '../gateway/events/user/user-logged-in.event';
+import { PlayerId } from '../gateway/shared-types/player-id';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller('auth/steam')
 export class SteamController {
   constructor(
     private readonly jwtService: JwtService,
     private readonly userRepository: UserRepository,
+    @Inject('QueryCore') private readonly rq: ClientProxy,
   ) {}
 
   @Get()
@@ -26,8 +31,17 @@ export class SteamController {
   async steamAuthRequest(@Req() req, @Res() res) {
     const steam32id = steam64to32(req.user!!._json.steamid);
 
+
+
+    this.rq.emit(UserLoggedInEvent.name, new UserLoggedInEvent(
+      new PlayerId(steam32id),
+      req.user!!._json.personaname,
+      req.user!!._json.avatarfull,
+    ))
+
     const existingUser = await this.userRepository.get(steam32id);
-    const numericalId = numSteamId(steam32id)
+    const numericalId = numSteamId(steam32id);
+
 
     if (existingUser) {
       const token = this.jwtService.sign({ sub: steam32id, roles: existingUser.roles });
