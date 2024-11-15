9ac3930752f9ad2dce543b93b567a67ab30ebc05
Yuri Stepin <enchantinggg4@gmail.com>
2024-11-15 09:03:40 +0300

    Removed extensive middleware

--- src/tracer.ts
+++ src/tracer.ts
@@ -1,6 +1,5 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import * as process from 'process';
-import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
 import { Resource } from '@opentelemetry/resources';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
@@ -11,7 +10,6 @@ import { JAEGER_EXPORT_URL } from './utils/env';
 import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
 import { FetchInstrumentation } from '@onreza/opentelemetry-instrumentation-fetch-bun';
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 
 const exporterOptions = {
   url: JAEGER_EXPORT_URL, // grcp
@@ -26,8 +24,8 @@ export const otelSDK = new NodeSDK({
     new FetchInstrumentation({
       propagateTraceHeaderCorsUrls: new RegExp('.*')
     }),
-    getNodeAutoInstrumentations(),
-    new HttpInstrumentation(),
+    // getNodeAutoInstrumentations(),
+    // new HttpInstrumentation(),
     new ExpressInstrumentation(),
     new NestInstrumentation(),
   ],
