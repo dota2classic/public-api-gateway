58a7286e000f70de442aaf8bf44c9755bf111032
Yuri Stepin <enchantinggg4@gmail.com>
2024-12-15 22:45:07 +0300

    Make userloggedinevent public

--- src/main.service.ts
+++ src/main.service.ts
@@ -1,16 +1,32 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable, OnApplicationBootstrap } from "@nestjs/common";
 import { GetAllQuery } from "./gateway/queries/GetAll/get-all.query";
 import { GetAllQueryResult } from "./gateway/queries/GetAll/get-all-query.result";
 import { UserRepository } from "./cache/user/user.repository";
 import { UserModel } from "./cache/user/user.model";
-import { QueryBus } from "@nestjs/cqrs";
+import { ofType, QueryBus } from "@nestjs/cqrs";
+import { ClientProxy } from "@nestjs/microservices";
+import { UserLoggedInEvent } from "./gateway/events/user/user-logged-in.event";
 
 @Injectable()
-export class MainService {
+export class MainService implements OnApplicationBootstrap {
   constructor(
     private readonly qbus: QueryBus,
+    private readonly ebus: QueryBus,
     private readonly userRepository: UserRepository,
+    @Inject("QueryCore") private readonly redisEventQueue: ClientProxy,
   ) {}
+
+  async onApplicationBootstrap() {
+    try {
+      await this.redisEventQueue.connect();
+    } catch (e) {}
+
+    const publicEvents: any[] = [UserLoggedInEvent];
+
+    this.ebus
+      .pipe(ofType(...publicEvents))
+      .subscribe((t) => this.redisEventQueue.emit(t.constructor.name, t));
+  }
 
   // @Cron("*/30 * * * * *")
   async actualizeServers() {
