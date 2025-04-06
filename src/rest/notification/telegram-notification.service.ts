9513f647280c891d05970d4cb052738e9a1b5af5
Yuri Stepin <enchantinggg4@gmail.com>
2025-04-06 17:46:40 +0300

    AAAA

--- src/rest/notification/telegram-notification.service.ts
+++ src/rest/notification/telegram-notification.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable, Logger } from "@nestjs/common";
 import TelegramBot from "node-telegram-bot-api";
 import { ConfigService } from "@nestjs/config";
 
