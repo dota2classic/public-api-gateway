4e8caa245bc9687bed5f007860dba0ff9f7fa524
Yuri Stepin <enchantinggg4@gmail.com>
2025-04-06 17:44:57 +0300

    Fix again

--- src/rest/notification/telegram-notification.service.ts
+++ src/rest/notification/telegram-notification.service.ts
@@ -4,31 +4,38 @@ import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class TelegramNotificationService {
+  private logger = new Logger(TelegramNotificationService.name);
   constructor(
     @Inject("Telegram") private readonly telegram: TelegramBot,
     private readonly config: ConfigService,
   ) {}
 
   public async notifyFeedback(text: string) {
-    await this.telegram.sendMessage(
-      this.config.get("telegram.notifyChatId"),
-      text,
-      {
-        message_thread_id: this.config.get("telegram.notifyThreadId"),
-      },
-    );
+    try {
+      await this.telegram.sendMessage(
+        this.config.get("telegram.notifyChatId"),
+        text,
+        {
+          message_thread_id: this.config.get("telegram.notifyThreadId"),
+        },
+      );
+    } catch (e) {
+      this.logger.error("Error sending to telegram", e);
+    }
   }
-
 
   public async notifyMarkdown(text: string) {
-    await this.telegram.sendMessage(
-      this.config.get("telegram.notifyChatId"),
-      text,
-      {
-        message_thread_id: this.config.get("telegram.notifyThreadId"),
-        parse_mode: "Markdown"
-      },
-    );
+    try {
+      await this.telegram.sendMessage(
+        this.config.get("telegram.notifyChatId"),
+        text,
+        {
+          message_thread_id: this.config.get("telegram.notifyThreadId"),
+          parse_mode: "Markdown",
+        },
+      );
+    } catch (e) {
+      this.logger.error("Error sending to telegram", e);
+    }
   }
-
 }
