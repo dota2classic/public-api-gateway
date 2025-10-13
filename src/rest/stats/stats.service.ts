0165be2a59342e065a8c0e54af702266eeaee1ca
Yuri Stepin <enchantinggg4@gmail.com>
2025-10-13 09:15:35 +0300

    Small fix

--- src/rest/stats/stats.service.ts
+++ src/rest/stats/stats.service.ts
@@ -22,7 +22,7 @@ export class StatsService implements OnApplicationBootstrap {
 
   @Cron(CronExpression.EVERY_5_MINUTES)
   private async refreshQueueStats() {
-    this._stats = await this.queueTimesChart();
+    // this._stats = await this.queueTimesChart();
     // console.log(JSON.stringify(this._stats))
   }
 
