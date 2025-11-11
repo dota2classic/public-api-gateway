51763014cbb99e6b088408a54f8a50b2d3ee3f84
Yuri Stepin <enchantinggg4@gmail.com>
2025-11-11 22:01:01 +0300

    5 min cache

--- src/rest/stats/stats.controller.ts
+++ src/rest/stats/stats.controller.ts
@@ -168,7 +168,7 @@ export class StatsController {
   }
 
   @UseInterceptors(GlobalHttpCacheInterceptor)
-  @CacheTTL(60 * 6)
+  @CacheTTL(60 * 5)
   @Get("/agg_stats")
   public async getAggStats(): Promise<AggregatedStatsDto> {
     return this.ms.infoControllerGetAggregatedStats();
