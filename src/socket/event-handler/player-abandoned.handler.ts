56acc7a42d0a6281d5d54c6bda87798a83d0ca48
Yuri Stepin <enchantinggg4@gmail.com>
2026-03-17 15:39:12 +0300

    Add logging

--- src/socket/event-handler/player-abandoned.handler.ts
+++ src/socket/event-handler/player-abandoned.handler.ts
@@ -1,14 +1,16 @@
 import { EventsHandler, IEventHandler } from "@nestjs/cqrs";
-import { PartyUpdatedEvent } from "../../gateway/events/party/party-updated.event";
 import { SocketDelivery } from "../socket-delivery";
 import { PartyService } from "../../party.service";
 import { MessageTypeS2C } from "../messages/s2c/message-type.s2c";
 import { SocketMessageService } from "../socket-message.service";
-import { PlayerQueueStateMessageS2C } from "../messages/s2c/player-queue-state-message.s2c";
 import { PlayerAbandonedEvent } from "../../gateway/events/bans/player-abandoned.event";
+import { Logger } from "@nestjs/common";
 
 @EventsHandler(PlayerAbandonedEvent)
-export class PlayerAbandonedSocketHandler implements IEventHandler<PlayerAbandonedEvent> {
+export class PlayerAbandonedSocketHandler
+  implements IEventHandler<PlayerAbandonedEvent>
+{
+  private logger = new Logger(PlayerAbandonedSocketHandler.name);
   constructor(
     private readonly delivery: SocketDelivery,
     private readonly partyService: PartyService,
@@ -16,7 +18,18 @@ export class PlayerAbandonedSocketHandler implements IEventHandler<PlayerAbandon
   ) {}
 
   async handle(event: PlayerAbandonedEvent) {
-    const gs = await this.socketMessage.playerGameState(event.playerId.value)
-    await this.delivery.deliver(event.playerId.value, MessageTypeS2C.PLAYER_GAME_STATE, gs)
+    const gs = await this.socketMessage.playerGameState(event.playerId.value);
+    this.logger.log(
+      "Received PlayerAbandonedEvent for " +
+        event.playerId.value +
+        ", emitting its gamestate: ",
+      gs,
+    );
+
+    await this.delivery.deliver(
+      event.playerId.value,
+      MessageTypeS2C.PLAYER_GAME_STATE,
+      gs,
+    );
   }
 }
